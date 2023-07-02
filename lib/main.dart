31314187a662c6a8a1ea33a6ac747b62aeb73216
AbdulSami10 <sami.sayani10@gmail.com>
2023-07-03 01:03:30 +0500

    init state method

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(color: Colors.purple),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  "assets/images/quiz-logo.png",
-                  width: 300,
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                const Text(
-                  "Learn Flutter The Fun way!",
-                  style: TextStyle(fontSize: 25, color: Colors.white),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                const TextButton(
-                  onPressed: null,
-                  child: Text(
-                    "Start Quiz",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+  runApp(Quiz());
 }
