06c10ad766e8b3a93e961b8c6ee707157bb49975
AbdulSami10 <sami.sayani10@gmail.com>
2023-07-03 15:54:18 +0500

    shuffle list

--- lib/model/quiz_question.dart
+++ lib/model/quiz_question.dart
@@ -2,4 +2,9 @@ class QuizQuestion {
   const QuizQuestion(this.text, this.answers);
   final String text;
   final List<String> answers;
+  List<String> getShuffledAnswer() {
+    final shuffledAnswer = List.of(answers);
+    shuffledAnswer.shuffle();
+    return shuffledAnswer;
+  }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/data/questions.dart';
+import 'package:quiz/model/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,7 +13,26 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    // TODO: implement build
-    return Text("Questions");
+    final currentQuestion = questions[0];
+    return SizedBox(
+        width: double.infinity,
+        child: Container(
+            margin: const EdgeInsets.all(40),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    currentQuestion.text,
+                    style: const TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ...currentQuestion.answers.map((item) {
+                    return AnswerButton(answerText: item, onTap: () {});
+                  }),
+                ])));
   }
 }
